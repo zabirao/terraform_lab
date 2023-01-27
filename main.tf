d613ecb1d06d15c52864baafcd756e7469799b85
Rao Shahzaib <zabirao29@gmail.com>
2023-01-27 11:33:58 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
-	access_key ="AKIA5DFKNNF7EP32FH4G"
-	secret_key = "AKIA5DFKNNF7EP32FH4G,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"
+	access_key ="AKIA5DFKNNF7PEH6GGXI"
+	secret_key = "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"
 	region = "ap-south-1"
 }
 
 resource "aws_s3_bucket" "b" {
 	bucket = "myawsbucket9600533"
-	ac1 = "private"
 
 	tags = {
 	Name  = "My bucket"
